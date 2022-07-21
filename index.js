8b76aff0a3ea15d2ae694c85ff05f9d74717bfd5
JHPatchouli <43368320+JHPatchouli@users.noreply.github.com>
2022-07-22 06:47:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,18 +6,22 @@ $(function () {
     d1.append('<div id="do' + index + '" style="height: 1%;"></div>');
     d2.append('<div id="dt' + index + '" style="height: 1%;"></div>');
   }
+  
   for (let index = 0; index < 20; index++) {
-    th(index,index*500,1-index*0.0005,"do");
+    var rds=Math.ceil(Math.random()*99);
+    th(index,rds*100,3.0-index*0.0005+rds/1000000,"do");
   }
   let j=0;
   for (let index = 20; index > -1; index--) {
-    th(index,j*500,1-j*0.0005,"dt");
+    var rds=Math.ceil(Math.random()*99);
+    th(index,rds*100,3.0-j*0.0005+rds/1000000,"dt");
     j++;
   }
 });
 async function th(idd, timing,sp,dn) {
   await sleep(timing);
   while (true) {
+     sp+=Math.ceil(Math.random()*99)/1000000;
     await sleep(0.1);
     var dotmp = $("#"+ dn + idd);
     var heighttmp = dotmp.height();
@@ -27,11 +31,13 @@ async function th(idd, timing,sp,dn) {
       break;
     }
   }
+sp+=Math.ceil(Math.random()*99)/1000000;
   thb(idd,timing,sp,dn);
 }
 
 async function thb(idd, timing,sp,dn) {
     while (true) {
+sp+=Math.ceil(Math.random()*99)/1000000;
       await sleep(0.1);
       var dotmp = $("#"+ dn + idd);
       var heighttmp = dotmp.height();
@@ -41,11 +47,13 @@ async function thb(idd, timing,sp,dn) {
         break;
       }
     }
+sp+=Math.ceil(Math.random()*99)/1000000;
     thg(idd,timing,sp,dn);
 }
 
 async function thg(idd, timing,sp,dn) {
     while (true) {
+sp+=Math.ceil(Math.random()*99)/1000000;
       await sleep(0.1);
       var dotmp = $("#"+ dn + idd);
       var heighttmp = dotmp.height();
@@ -55,5 +63,6 @@ async function thg(idd, timing,sp,dn) {
         break;
       }
     }
+sp+=Math.ceil(Math.random()*99)/1000000;
     thb(idd,timing,sp,dn);
   }
